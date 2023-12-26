570dde7e75732c4116d5b8fb174450e2d86e1452
Nadhir Djabali <79930796+nadhiro7@users.noreply.github.com>
2023-12-26 16:04:49 +0100

    Create Config Server

--- cloud-config-server/src/test/java/com/tp9al/cloud/config/CloudConfigServerApplicationTests.java
+++ cloud-config-server/src/test/java/com/tp9al/cloud/config/CloudConfigServerApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.configserver;
+package com.tp9al.cloud.config;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ConfigServerApplicationTests {
+class CloudConfigServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
